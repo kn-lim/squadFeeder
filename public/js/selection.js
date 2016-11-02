92223a97bbd3eb365339280437263dd771c21439
Arlen Pan <arlenpan@gmail.com>
2016-11-02 16:29:39 -0700

    some layout done

--- public/js/selection.js
+++ public/js/selection.js
@@ -3,7 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
