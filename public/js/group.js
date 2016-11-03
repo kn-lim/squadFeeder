f73673d2e4e9873852f6e3f9991b5474c6bbe172
Nicholas Pung <npung@ucsd.edu>
2016-11-03 13:47:05 -0700

    broke javascript before, clipboard function works again

--- public/js/group.js
+++ public/js/group.js
@@ -14,12 +14,3 @@ function initializePage() {
 }
 
 // Clipboard script
-var clipboard = new Clipboard('.btn');
-
-clipboard.on('success', function(e) {
-    console.log(e);
-});
-
-clipboard.on('error', function(e) {
-    console.log(e);
-});
