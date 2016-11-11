aaad9aa3e740268570957aca8cd8c2506e7abc1d
Kevin Lim <knlim95@gmail.com>
2016-11-10 16:04:56 -0800

    Yelp - Attempt #2.4

--- public/js/results.js
+++ public/js/results.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+    yelp_init();
 })
 
 /*
@@ -117,3 +118,18 @@ function initialize() {
     //     google.maps.event.removeListener(boundsListener);
     // });
 }
+
+/* Grab access token and token type from Yelp */
+function yelp_init() {
+    $.post("https://api.yelp.com/oauth2/token", {
+        client_id: "XSB11XkGiPzzB6Oq3rJ77A",
+        client_secret: "2XtQUalVyB6z6Ety9veg5qICLMQpmobGZGz9cqrlUms8FtqIwo2h6uxOTWeoVODn",
+        grant_type: "client_credentials"
+        },
+        function(response) {
+            console.log("Yelp Authorization Successful ", response);
+
+            window.localStorage.setItem("token", response.access_token);
+            window.localStorage.setItem("token_type", response.token_type);
+        });
+};
